146b6c4a0dfb270527d4959802fb46f7c739c3db
gauravshegokar93 <gauravshegokar2312@gmail.com>
2024-12-07 17:02:15 +0530

    refextor - add var

---
+++ 01_basic/variable.js
@@ -0,0 +1,15 @@
+const accoutId = 12345
+var accoutEmail = "gauravshegokar@gmail.com"
+let password = "@gaurav"
+accoutCity = "Shegaon"
+let accoutState;
+
+ accoutEmail = "contact.gauravshegokar@gmail.com"
+ password = "@Gaurav"
+ accoutCity = "pune"
+
+ /* var not use because issue in bloack scope */
+
+console.log(accoutId) 
+
+console.table([accoutEmail,password,accoutCity,accoutState])
