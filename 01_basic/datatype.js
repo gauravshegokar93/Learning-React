6cea9fad9371c5f4c494b68c24c18996a37ae697
gauravshegokar93 <gauravshegokar2312@gmail.com>
2024-12-11 00:00:22 +0530

    refeactor

---
+++ 01_basic/datatype.js
@@ -0,0 +1,31 @@
+"use strick" ; // trate all code of js in newer version
+
+alert(3 +3) //  we are using node.js not brower
+
+// number = 2
+// string = "string"
+// boolen = "true,flase"
+//undefine
+//null = stantalone value
+// symbol 
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25")
